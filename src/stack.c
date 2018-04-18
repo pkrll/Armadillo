200313d16c702b3f812be643c29fe561176a9382
Fredrik Jonasson <fredrik.jonasson@outlook.com>
2018-04-18 14:41:09 +0200

    Pekarbus

--- src/stack.c
+++ src/stack.c
@@ -1,76 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+#include<stdlib.h>  // Ska raderas!
+#include<stdio.h> 	// Ska raderas!
 
-// A struct that represents the stack.
+struct Stack_element {
+	void *element;
+	void *element_next;
+	void *element_last;
+} typedef Stack_element_t;
+
 struct Stack {
-	void *stack_content;
-	int top;
-	int maximum_size;
-	int elem_size;
+	Stack_element_t element;
+	Stack_element_t top;
+	int size;
 } typedef Stack_t;
 
-// A function that initialize the Stack.
-Stack_t *initialize_stack(int elem_size, int maximum_size) {
-	Stack_t *stack_pointer = calloc(1, sizeof (*stack_pointer));
-	stack_pointer->top = -1;
-	stack_pointer->maximum_size=maximum_size;
-	stack_pointer->elem_size = elem_size;
-	stack_pointer->stack_content=calloc(1, sizeof(elem_size));
+Stack_t* initialize_stack() {
+	Stack_t *stack_pointer  = calloc(1, sizeof(stack_pointer));
+	//stack_pointer -> top = NULL;
+	stack_pointer -> size = 0;
 	return stack_pointer;
 }
 
-// A function that destroys the stack and gives the memory back.
-int destroy_stack(Stack_t *stack_pointer) {
-	free(stack_pointer->stack_content);
-  free(stack_pointer);
-  return 0;
-}
-// A function	that checks if the Stack is empty.
-bool check_if_empty(Stack_t *stack_pointer) {
-	if (stack_pointer->top==-1) {
-		return true;
-	} else {
-		return false;
-	}
+Stack_t* push_to_stack(Stack_t *stack_pointer, void *element) {
+	Stack_element_t *new_element = calloc(1, sizeof(element));
+	stack_pointer ->  element  = *new_element;
+	stack_pointer ->  top  = *new_element;
+	stack_pointer -> size = ((stack_pointer -> size)+1);
+
+	new_element -> element = new_element;
+	new_element -> element_next = NULL;
+	new_element -> element_last = NULL;
+	return stack_pointer;
 }
 
-// A function that checks if the Stack is full.
-bool check_if_full(Stack_t *stack_pointer) {
-	if((stack_pointer->top) == ((stack_pointer->maximum_size)-1)) {
-		return  true;
-	} else {
-		return false;
-	}
-}
-
-// A function that pushes an element to the Stack.
-int push_to_stack(Stack_t *stack_pointer, void *elem) {
-	if(check_if_full) {
-		printf("Stack is full\n");
-		return 0;
-	}
-	stack_pointer->top++;
-	void *size_holder = (char *)stack_pointer->stack_content+(stack_pointer->top*stack_pointer->elem_size);
-	memcpy(size_holder, elem, stack_pointer->elem_size);
-	return 0;
-}
-
-// A function that pops an element from the Stack.
-int pop_from_stack(Stack_t *stack_pointer, void *target) {
-	if(check_if_empty) {
-		printf("Stack is empty\n");
-	}
-	void* source = (char*)stack_pointer->stack_content+(stack_pointer->top*stack_pointer->elem_size);
-	memcpy(target, source, stack_pointer->elem_size);
-	return 0;
+Stack_element_t pop_from_stack(Stack_t *stack_pointer) {
+	Stack_element_t popped_object = stack_pointer -> top;
+	// Continue implementing here.
+	return popped_object;
 }
 
 int main(int argc, char const *argv[]) {
-	Stack_t *test_stack=initialize_stack(sizeof(int), 5);
-	push_to_stack(test_stack, (void*)1);
-	push_to_stack(test_stack, (void*)2);
-	printf("Stack created and pushed\n");
+	Stack_t *stack = initialize_stack();
+	if (stack != NULL) {
+		printf("Sucess\n");
+	} else {
+	printf("failure\n");
+	}
+	int f = 1; // Vi skapar en pekare *f som pekar på "F".
+	push_to_stack(stack, &f); // Vi pushar pekaren till stacken.
+	int *hej = ((int*)(pop_from_stack(stack))); // Vill poppa f-pekaren från stacken och spara den i char.
+	if (*hej==1) {
+		printf("Double Sucess\n");
+	} else {
+		printf("Failure\n");
+	}
 	return 0;
 }
