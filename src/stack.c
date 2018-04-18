44e8380198d586ac713536e00a8edad96f2f41be
Fredrik Jonasson <fredrik.jonasson@outlook.com>
2018-04-18 15:23:55 +0200

    Fortsätt d-bugga

--- src/stack.c
+++ src/stack.c
@@ -4,12 +4,10 @@
 struct Stack_element {
 	void *element;
 	void *element_next;
-	void *element_last;
 } typedef Stack_element_t;
 
 struct Stack {
-	Stack_element_t element;
-	Stack_element_t top;
+	Stack_element_t *top;
 	int size;
 } typedef Stack_t;
 
@@ -22,18 +20,16 @@ Stack_t* initialize_stack() {
 
 Stack_t* push_to_stack(Stack_t *stack_pointer, void *element) {
 	Stack_element_t *new_element = calloc(1, sizeof(element));
-	stack_pointer ->  element  = *new_element;
-	stack_pointer ->  top  = *new_element;
+	stack_pointer ->  top  = element;
 	stack_pointer -> size = ((stack_pointer -> size)+1);
 
 	new_element -> element = new_element;
 	new_element -> element_next = NULL;
-	new_element -> element_last = NULL;
 	return stack_pointer;
 }
 
-Stack_element_t pop_from_stack(Stack_t *stack_pointer) {
-	Stack_element_t popped_object = stack_pointer -> top;
+void* pop_from_stack(Stack_t *stack_pointer) {
+	void* popped_object = (stack_pointer -> top)->element;
 	// Continue implementing here.
 	return popped_object;
 }
@@ -45,13 +41,13 @@ int main(int argc, char const *argv[]) {
 	} else {
 	printf("failure\n");
 	}
-	int f = 1; // Vi skapar en pekare *f som pekar på "F".
-	push_to_stack(stack, &f); // Vi pushar pekaren till stacken.
-	int *hej = ((int*)(pop_from_stack(stack))); // Vill poppa f-pekaren från stacken och spara den i char.
+	int f = 1;
+	push_to_stack(stack, &f);
+	int *hej = ((pop_from_stack(stack)));
 	if (*hej==1) {
-		printf("Double Sucess\n");
+		printf("Sucessfull push and pop\n");
 	} else {
-		printf("Failure\n");
+		printf("Failure push and pop\n");
 	}
 	return 0;
 }
