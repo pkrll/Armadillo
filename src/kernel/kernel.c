de0ce6bd45b17fa5b71831ac94663ca205d58bcc
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-04-18 10:33:23 +0200

    Working on exceptions

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -41,7 +41,7 @@ void print_to_led(const int position) {
 
 void start_counter() {
 	while (1) {
-		for (int i = 0; i < 10; i++) {
+		for (int i = 1; i < 10; i++) {
 			for (int i = 0; i < 100000000; i++) { /* Pause... */ }
 
 			if (i == 9) {
@@ -57,7 +57,11 @@ void start_counter() {
 int main(void) {
 	printk("Hello world!\n");
 	print_to_ascii("UphillOS");
-	start_counter();
+	// start_counter();
 
 	return 0;
+}
+
+void exception_handler() {
+	printk("Nope");
 }
