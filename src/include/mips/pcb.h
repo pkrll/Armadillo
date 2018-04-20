0af55cc979a5dfab3fbf72a0a42e2ac3f30a0099
Ulf Sigvardsson <ulf_sigvardsson@hotmail.com>
2018-04-20 19:36:04 +0200

    PCB's sorta implemented, rudimentary version of a context switch too.

--- src/include/mips/pcb.h
+++ src/include/mips/pcb.h
@@ -1,8 +1,8 @@
 #ifndef __PCB_H_
 #define __PCB_H_
 
-#define PID_SIZE        0x1
-#define STATE_SIZE      0x1
+#define PID_SIZE        0x4
+#define STATE_SIZE      0x4
 #define CONTEXT_SIZE    0x6C      //  Size of $v0, $v1, $a0-$a3, $t0-$t9, $s0-$s7, $gp, $fp, $ra
 #define STACK_SIZE      0x100000  //  1MB stack
 #define TEXT_SIZE       0x100000  //  1MB .text segment
@@ -14,4 +14,5 @@
 
 #define PCB_SIZE PID_SIZE + STATE_SIZE + CONTEXT_SIZE + MEM_IMAGE_SIZE + SCHEDULE_SIZE + PC_SIZE + SP_SIZE
 
+#define CONTEXT_OFFSET PID_SIZE + STATE_SIZE
 #endif
