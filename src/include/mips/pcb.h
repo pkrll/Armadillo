40f474bad58f7900c5b22c9e8f5854f76e1bde31
Ulf Sigvardsson <ulf_sigvardsson@hotmail.com>
2018-04-20 22:13:39 +0200

    Minor bug fixing

--- src/include/mips/pcb.h
+++ src/include/mips/pcb.h
@@ -3,16 +3,15 @@
 
 #define PID_SIZE        0x4
 #define STATE_SIZE      0x4
-#define CONTEXT_SIZE    0x6C      //  Size of $v0, $v1, $a0-$a3, $t0-$t9, $s0-$s7, $gp, $fp, $ra
+#define CONTEXT_SIZE    0x70      //  Size of $v0, $v1, $a0-$a3, $t0-$t9, $s0-$s7, $gp, $fp, $ra, $sp
 #define STACK_SIZE      0x100000  //  1MB stack
 #define TEXT_SIZE       0x100000  //  1MB .text segment
 #define DATA_SIZE       0x100000  //  1MB .data segment
 #define MEM_IMAGE_SIZE  STACK_SIZE + TEXT_SIZE + DATA_SIZE
 #define SCHEDULE_SIZE   0x8       //  Next and previous pointers
-#define PC_SIZE         0x4
-#define SP_SIZE         0x4
 
-#define PCB_SIZE PID_SIZE + STATE_SIZE + CONTEXT_SIZE + MEM_IMAGE_SIZE + SCHEDULE_SIZE + PC_SIZE + SP_SIZE
+#define PCB_SIZE PID_SIZE + STATE_SIZE + CONTEXT_SIZE + MEM_IMAGE_SIZE + SCHEDULE_SIZE
 
 #define CONTEXT_OFFSET PID_SIZE + STATE_SIZE
+#define PC_OFFSET PID_SIZE + STACK_SIZE + CONTEXT_SIZE + STACK_SIZE + TEXT_SIZE + DATA_SIZE + MEM_IMAGE_SIZE + SCHEDULE_SIZE
 #endif
