1d0d0d036485195fdfb711b7e8d98b7b3576ff02
Love Osslund <love.osslund@gmail.com>
2018-04-27 17:56:06 +0200

    free fixed, some test in main

--- src/common/memory.c
+++ src/common/memory.c
@@ -1,6 +1,7 @@
 #include <common/memory.h>
 
-#define PAGESIZE 4096
+#define PAGESIZE 8
+#define ALIGNMEM 8
 
 extern uint8_t __end __attribute__((section (".data")));
 
@@ -21,6 +22,11 @@ typedef struct FreeSegment {
 
 void *find_first_free(size_t size);
 int number_of_pages(size_t size);
+size_t aligned_mem(size_t mem);
+
+void do_nothing(void *ptr) {
+  ptr = ptr + 0;
+}
 
 void mem_init() {
 	metadata_t *metadata = (metadata_t *)((uint32_t)&__end);
@@ -37,16 +43,18 @@ void *malloc(size_t size) {
 
 	if (free_allocation_list_head == NULL) {
 		last = metadata->last_allocation;
-		metadata->last_allocation += number_of_pages(size) * PAGESIZE + sizeof(segment_t);
+                size_t unaligned_memory = number_of_pages(size) * PAGESIZE + sizeof(segment_t);
+                size_t aligned_memory = aligned_mem(unaligned_memory);
+		metadata->last_allocation += aligned_memory;
+                last->size = number_of_pages(aligned_memory);
 	} else {
-		metadata->free_allocation_list_head = free_allocation_list_head->next;
-		free_allocation_list_head->next = NULL;
-		last = (segment_t *)free_allocation_list_head;
+          metadata->free_allocation_list_head = free_allocation_list_head->next;
+          free_allocation_list_head->next = NULL;
+          last = (segment_t *)free_allocation_list_head;
 	}
 
 	// Move pointer forward sizeof(segment_t)
 	if (last) {
-		last->size = number_of_pages(size);
 		last += sizeof(segment_t);
 	}
 
@@ -57,10 +65,24 @@ void free(void *ptr) {
 	if (ptr == NULL) return;
 
 	metadata_t *metadata = (metadata_t *)((uint32_t)&__end);
-	free_segment_t *segment = (free_segment_t *)ptr;
-
+	free_segment_t *segment = (free_segment_t *)(ptr - sizeof(segment));
+        
 	segment->next = metadata->free_allocation_list_head;
-	metadata->free_allocation_list_head = segment;
+        metadata->free_allocation_list_head = segment;
+}
+
+size_t aligned_mem(size_t mem) {
+  if (mem % ALIGNMEM == 0) {
+    return mem;
+    // Already alligned
+  } else {
+    return mem + (ALIGNMEM -(mem % ALIGNMEM));
+    // Align memory with ALIGNMEM
+  }
+}
+
+int number_of_pages(size_t size) {
+  return size / PAGESIZE; 
 }
 
 void *find_first_free(size_t size) {
@@ -69,7 +91,7 @@ void *find_first_free(size_t size) {
 
 	while (head) {
 		// TODO: This just takes the first segment that fits, need to optimize.
-		if (head->size > size) {
+          if ((head->size * PAGESIZE)  >= size) {
 			return head;
 		}
 
@@ -79,6 +101,3 @@ void *find_first_free(size_t size) {
 	return NULL;
 }
 
-int number_of_pages(size_t size) {
-	return size / PAGESIZE + 1;
-}
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -67,11 +67,50 @@ int main(void) {
 	printk(array);
 
 	char *array2 = malloc(sizeof(char)*3);
-	array2[0] = 'd';
+	array2[0] = 'b';
 	array2[1] = '\n';
 	array2[2] = '\0';
 	printk(array2);
-	printk(array);
+        
+        char *array3 = malloc(sizeof(char)*3);
+	array3[0] = 'c';
+	array3[1] = '\n';
+	array3[2] = '\0';
+	printk(array3);
+        
+        free(array3);
+        
+        char *array4 = malloc(sizeof(char)*3);
+	array4[0] = 'X';
+	array4[1] = '\n';
+	array4[2] = '\0';
+        
+	printk(array3);
+        // Should print 'X', which has been written at the adress of array2 ('c').
+        
+        free(array2);
+        free(array);
+
+        char *nothing = malloc(sizeof(char)*20);
+        nothing += 1;
+        // Should use 'new' memory since 20 > 8, which is the size of the freed blocks 'array' and 'array2'.
+        
+        char *array5 = malloc(sizeof(char)*3);
+	array5[0] = 'A';
+	array5[1] = '\n';
+	array5[2] = '\0';
+        
+        char *array6 = malloc(sizeof(char)*3);
+	array6[0] = 'B';
+	array6[1] = '\n';
+	array6[2] = '\0';
+
+        printk(array);
+        // Should print 'A', which has been written at adress of array ('a').
+        printk(array2);
+        // Should print 'B', which has been written at adress of array ('b').
+
+        
 
 	return 0;
 }
