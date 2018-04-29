0792df03de0614af37c6ddda6a202c574660e7e3
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-04-29 18:06:55 +0200

    Added tests for strcmp

--- tests/test_stdlib.c
+++ tests/test_stdlib.c
@@ -5,14 +5,24 @@
 int tests_run __attribute__((section (".data"))) = 0;
 
 static char *test_strlen() {
-  mu_assert("error, strlen(\"Hello\") != 5", strlen("Hello") == 5);
-  mu_assert("error, strlen(\"\") != 0", strlen("") == 0);
+  mu_assert("Test failed: strlen(\"Hello\") != 5", strlen("Hello") == 5);
+  mu_assert("Test failed: strlen(\"\") != 0", strlen("") == 0);
+  mu_assert("Test failed: strlen(\"foo\\0\") != 3", strlen("foo\0") == 3);
+
+	return 0;
+}
+
+static char *test_strcmp() {
+	mu_assert("Test failed: strcmp(\"ABC\", \"ABC\") != 0", strcmp("ABC", "ABC") == 0);
+	mu_assert("Test failed: strcmp(\"ABC\", \"BBC\") !< 0", strcmp("ABC", "BBC") < 0);
+	mu_assert("Test failed: strcmp(\"BBC\", \"ABC\") !> 0", strcmp("BBC", "ABC") > 0);
 
 	return 0;
 }
 
 static char *all_tests() {
 	mu_run_test(test_strlen);
+	mu_run_test(test_strcmp);
 
 	return 0;
 }
