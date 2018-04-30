dfbc78219740485421f64c10ddc6d04ac4fa3820
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-04-30 22:45:22 +0200

    Added more macros

--- src/include/common/math.h
+++ src/include/common/math.h
@@ -20,5 +20,26 @@
  * @return   f rounded down to its largest integral value.
  */
 #define floor(f) 	__builtin_floor ((f))
+/**
+ * Returns the smallest integer value greater than or equal to x.
+ *
+ * @param  f The floating point number to round up.
+ * @return   The smallest integer value greater than or equal to x.
+ */
+#define ceil(f)		__builtin_ceil	((f))
+/**
+ * Calculates the value of e raised to the power of x.
+ *
+ * @param  x The value to raise e by.
+ * @return   The value of e rasied to the power of x.
+ */
+#define exp(x)		__builtin_exp		((x))
+/**
+ * Returns the common logarithm (base-10 logarithm) of x.
+ *
+ * @param  x Value whose logarithm is calculated.
+ * @return   The common logarithm of x.
+ */
+#define log10(x)	__builtin_log10		((x))
 
 #endif
