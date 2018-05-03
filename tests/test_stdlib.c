087ebf0491107ef3d1caeeaf591ee1647a988dc3
Fredrik Jonasson <fredrik.jonasson@outlook.com>
2018-05-03 21:02:15 +0200

    Done some (ugly) testing of number_to_char, but gets a dispatch-error during compile. Need some reviewing

--- tests/test_stdlib.c
+++ tests/test_stdlib.c
@@ -18,9 +18,19 @@ static char *test_strcmp() {
 	return 0;
 }
 
+static char *test_number_to_char() {
+	mu_assert("Test failed: number_to_char(\"0\") != '0'", *number_to_char(0) == '0');
+	mu_assert("Test failed: number_to_char(\"1234\")[2] != '1234'", number_to_char(1234)[2] == '3');
+	mu_assert("Test failed: number_to_char(\"1034\")[1] != '1034'", number_to_char(1034)[1] == '0');
+	mu_assert("Test failed: number_to_char(\"1204\")[3] != '1204'", number_to_char(1204)[3] == '4');
+	mu_assert("Test failed: number_to_char(\"1230\")[0] != '1230'", number_to_char(1230)[0] == '1');
+
+	return 0;
+}
+
 char *test_stdlib() {
 	mu_run_test(test_strlen);
 	mu_run_test(test_strcmp);
-
+	mu_run_test(test_number_to_char);
 	return 0;
 }
