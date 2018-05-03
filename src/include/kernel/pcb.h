c445d27321ff36f20c9096da5560e2f4a900d512
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-05-03 13:27:29 +0200

    Fixed documentation for pcb

--- src/include/kernel/pcb.h
+++ src/include/kernel/pcb.h
@@ -8,8 +8,8 @@ typedef int pid_t;
 typedef void *addr_t;
 
 /**
-* Enum representing the current state of a process
-*/
+ * Enum representing the current state of a process
+ */
 typedef enum State{
   ready = 0,
   running = 1,
@@ -19,11 +19,52 @@ typedef struct Pcb pcb_t;
 typedef struct Context context_t;
 typedef struct Stack stack_t;
 
+/**
+ * Allocates space for and initializes the starting state of a process.
+ *
+ * @param  pid 						A process id
+ * @param  start_function A function pointer to the starting function of the process
+ *
+ * @return Pointer to the resulting PCB
+ */
 pcb_t *init_pcb(pid_t pid, addr_t start_function);
+/**
+ * Allocates space for the CPU context of a PCB.
+ *
+ * @return Pointer to a CPU context.
+ */
 context_t *init_context();
-addr_t init_stack();
+/**
+ * Allocates space for a process stack.
+ *
+ * @return Pointer to a stack space.
+ */
+stack_t init_stack();
+/**
+ * Sets the state of a process.
+ *
+ * @param pcb   Pointer to a PCB.
+ * @param state New state of the process.
+ */
 void set_pcb_state(pcb_t *pcb, state_t state);
+/**
+ * Returns the PID of a process.
+ *
+ * @param  pcb A PCB.
+ * @return The PID of the process.
+ */
 pid_t get_pid(pcb_t *pcb);
+/**
+ * Returns the CPU context of a process.
+ * @param  pcb A PCB.
+ * @return Pointer to a CPU context.
+ */
 context_t *get_context(pcb_t *pcb);
+/**
+ * Returns the PC of a process.
+ * @param  pcb A PCB.
+ * @return The current PC of the process
+ */
 addr_t get_pc(pcb_t *pcb);
+
 #endif
--- src/kernel/pcb.c
+++ src/kernel/pcb.c
@@ -2,123 +2,97 @@
 
 #define PROCESS_STACKSIZE 512
 
+// -------------------------------
+// Structs
+// -------------------------------
+
 struct Stack {
-  void *start;
-};
-/**
-* Struct containing all registers to be saved for a given process
-*/
-struct Context{
-  reg_t sp;
-  reg_t ra;
-  reg_t t0;
-  reg_t t1;
-  reg_t t2;
-  reg_t t3;
-  reg_t t4;
-  reg_t t5;
-  reg_t t6;
-  reg_t t7;
-  reg_t t8;
-  reg_t t9;
-  reg_t v1;
-  reg_t v0;
-  reg_t a0;
-  reg_t a1;
-  reg_t a2;
-  reg_t a3;
-  reg_t s0;
-  reg_t s1;
-  reg_t s2;
-  reg_t s3;
-  reg_t s4;
-  reg_t s5;
-  reg_t s6;
-  reg_t s7;
-  reg_t gp;
-  reg_t fp;
-  reg_t at;
+	void *start;
 };
 
 /**
-* The entire PCB of a process
-*/
-struct Pcb{
-  pid_t pid;
-  addr_t pc;
-  state_t process_state;
-  context_t *cpu_context;
-  addr_t stack;
+ * Struct containing all registers to be saved for a given process
+ */
+struct Context {
+	reg_t sp;
+	reg_t ra;
+	reg_t t0;
+	reg_t t1;
+	reg_t t2;
+	reg_t t3;
+	reg_t t4;
+	reg_t t5;
+	reg_t t6;
+	reg_t t7;
+	reg_t t8;
+	reg_t t9;
+	reg_t v1;
+	reg_t v0;
+	reg_t a0;
+	reg_t a1;
+	reg_t a2;
+	reg_t a3;
+	reg_t s0;
+	reg_t s1;
+	reg_t s2;
+	reg_t s3;
+	reg_t s4;
+	reg_t s5;
+	reg_t s6;
+	reg_t s7;
+	reg_t gp;
+	reg_t fp;
+	reg_t at;
 };
 
-/*
-*	Allocates space for and initializes the starting state of a process
-* @param [in] pid A process id
-* @param [in] start_function A function pointer to the starting function of the process
-* @param [out] Pointer to the resulting PCB
-*/
-pcb_t *init_pcb(pid_t pid, addr_t start_function){
-  pcb_t *pcb = malloc(sizeof(pcb_t));
-  pcb->pid = pid;
-  pcb->pc = start_function;
-  pcb->process_state = 0;
-  pcb->cpu_context = init_context();
-  pcb->stack = init_stack();
-  pcb->cpu_context->sp = (reg_t)pcb->stack+PROCESS_STACKSIZE;
-  return pcb;
+/**
+ * The entire PCB of a process
+ */
+struct Pcb {
+	pid_t pid;
+	addr_t pc;
+	state_t process_state;
+	context_t *cpu_context;
+	addr_t stack;
 };
 
-/*
-*	Allocates space for the CPU context of a PCB
-* @param [out] Pointer to a CPU context
-*/
-context_t *init_context(){
-  context_t *context = malloc(sizeof(context_t));
-  return context;
+// -------------------------------
+// Public
+// -------------------------------
+
+pcb_t *init_pcb(pid_t pid, addr_t start_function) {
+	pcb_t *pcb = malloc(sizeof(pcb_t));
+	pcb->pid = pid;
+	pcb->pc = start_function;
+	pcb->process_state = 0;
+	pcb->cpu_context = init_context();
+	pcb->stack = init_stack();
+	pcb->cpu_context->sp = (reg_t)pcb->stack+PROCESS_STACKSIZE;
+	return pcb;
 }
 
-/*
-*	Allocates space for a process stack
-* @param [out] Pointer to a stack space
-*/
-addr_t init_stack(){
-  stack_t *stack = malloc(PROCESS_STACKSIZE);
-  return stack;
+context_t *init_context() {
+	context_t *context = malloc(sizeof(context_t));
+	return context;
 }
 
-/*
-*	@brief  Sets the state of a process
-*
-* @param [in] pcb Pointer to a PCB
-* @param [in] state New state of the process
-*/
-void set_pcb_state(pcb_t *pcb, state_t state){
-  pcb->process_state = state;
+stack_t init_stack() {
+	stack_t *stack = malloc(PROCESS_STACKSIZE);
+	return stack;
 }
 
-/*
-*	Returns the PID of a process
-* @param [in] pcb A PCB
-* @param [out] The PID of the process
-*/
-pid_t get_pid(pcb_t *pcb){
-  return pcb->pid;
+void set_pcb_state(pcb_t *pcb, state_t state) {
+	pcb->process_state = state;
 }
 
-/*
-*	Returns the CPU context of a process
-* @param [in] pcb A PCB
-* @param [out] Pointer to a CPU context
-*/
-context_t *get_context(pcb_t *pcb){
-  return pcb->cpu_context;
+pid_t get_pid(pcb_t *pcb) {
+	return pcb->pid;
 }
 
-/*
-*	Returns the PC of a process
-* @param [in] pcb A PCB
-* @param [out] The current PC of the process
-*/
-addr_t get_pc(pcb_t *pcb){
-  return pcb->pc;
+context_t *get_context(pcb_t *pcb) {
+	return pcb->cpu_context;
+}
+
+addr_t get_pc(pcb_t *pcb) {
+	return pcb->pc;
 }
