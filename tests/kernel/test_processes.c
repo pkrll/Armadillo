b6feb40dbbb858eb51072cc76faab44f06ae58d8
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-05-30 11:03:46 +0200

    Fixed tests for dispatcher

---
+++ tests/kernel/test_processes.c
@@ -0,0 +1,113 @@
+#include <common/processes.h>
+#include <common/stdlib.h>
+#include <kernel/dispatcher.h>
+#include <common/k_rand.h>
+#include <kernel/pcb.h>
+
+#define wait(x) for (int _i = 0; _i < x; _i++);
+
+extern int __terminated();
+
+void *processes[4] = {
+	process_1,
+	triangle_numbers,
+	square_numbers,
+	fibonacci
+};
+
+void delay() {
+	for (int i = 0; i < 250000000; i++);
+}
+
+void exit() {
+	asm volatile("li $a0, 99");
+	asm volatile("syscall");
+}
+
+void launch() {
+	int random = 0;
+
+	for (int i = 0; i < 4; i++) {
+		random = krand(1000000);
+		wait(random);
+
+		printf("\n==================\nSpawning process %d\n==================\n", i);
+		spawn_process(processes[i]);
+		void (*func)() = processes[i];
+		func();
+
+	}
+
+	printf("\n==================\nLaunch is done!\n==================\n");
+}
+
+void process_1() {
+	printf("\n==================\nPrinting 1..10\n==================\n");
+	for (int i = 0; i < 10; i++) {
+		int random = krand(100000000);
+		for (int i = 0; i < random; i++); //delay
+		printf("%d, ", i+1);
+	}
+
+	printf("\n==================\nDone printing 1..10\n==================\n");
+
+	exit();
+}
+
+void fibonacci() {
+	int n = 47; // More than that we get overflow...
+	unsigned long f1 = 0;
+	unsigned long f2 = 1;
+	unsigned long next = 0;
+
+	printf("\n==================\nPrinting the first %d terms of the Fibonacci series...\n==================\n", n);
+	for (int i = 0; i < n; i++) {
+		printf("%d, ", f1);
+		next = f1 + f2;
+		f1 = f2;
+		f2 = next;
+
+		int random = krand(100000000);
+		wait(random);
+	}
+
+	printf("\n==================\nDone printing the fibonacci series\n==================\n\n");
+
+	// spawn_process(fibonacci);
+
+	exit();
+}
+
+void square_numbers() {
+	int n = 1000;
+	int x = 0;
+	int s = 1;
+	printf("\n==================\nPrinting the square numbers\n==================\n");
+	for (int i = 0; i < n; i++, x = 2 * i + 1) {
+		s = s + x;
+		printf("%d ", s);
+
+		int random = krand(100000000);
+		wait(random);
+	}
+
+	printf("\n==================\nDone printing the square numbers\n==================\n\n");
+	exit();
+}
+
+void triangle_numbers() {
+	int n = 1000;
+	int last = 1;
+	printf("\n==================\nPrinting the triangle numbers\n==================\n\n");
+	for (int i = 0; i < n; i++) {
+		last = last + i;
+		printf("%d ", i + last);
+
+		int random = krand(100000000);
+		wait(random);
+	}
+
+	printf("\n==================\nDone printing the triangle numbers\n==================\n\n");
+
+	exit();
+}
--- tests/test_dispatcher.c
+++ tests/test_dispatcher.c
@@ -1,9 +1,10 @@
 #include <minunit/unit.h>
 #include <kernel/dispatcher.h>
+#include <common/processes.h>
+#include <common/queue.h>
+#include <kernel/pcb.h>
 #include <test_dispatcher.h>
 #include <dispatcher.c>
-#include <pcb.c>
-#include <queue.c>
 
 static char *test_dispatcher_init() {
 	mem_init();
@@ -16,16 +17,14 @@ static char *test_dispatcher_init() {
 	pcb_t *p0_test = running_process;
 
 	mu_assert("Test failed: We don't have a running process after dispatcher_init", running_process != NULL);
-	mu_assert("Test failed: p0 pid isn't correct", p0_test->pid == 0);
-	mu_assert("Test failed: p0 starting pc isn't correct", p0_test->pc == process_0);
-	mu_assert("Test failed: p0 process_state isn't 1 (running)", p0_test->process_state == 1);
+	mu_assert("Test failed: p0 pid isn't correct", get_pid(p0_test) == 1);
 
-	mu_assert("Test failed: p0 doesn't have an address to its cpu_context", p0_test->cpu_context != NULL);
-	mu_assert("Test failed: p0 doesn't have a stack", p0_test->stack != NULL);
+	mu_assert("Test failed: p0 starting pc isn't correct", get_pc(p0_test) == process_1);
+	mu_assert("Test failed: p0 process_state isn't 1 (running)", get_pcb_state(p0_test) == 1);
 
-	mu_assert("Test failed: p0 sp isn't correct", p0_test->cpu_context->sp == (reg_t)(p0_test->stack + PROCESS_STACKSIZE));
+	mu_assert("Test failed: p0 doesn't have an address to its cpu_context", get_context(p0_test) != NULL);
 
-	mu_assert("Test failed: ready_queue is empty", ready_queue->first != NULL);
+	mu_assert("Test failed: ready_queue is empty", queue_front(ready_queue) != NULL);
 	return 0;
 }
 
